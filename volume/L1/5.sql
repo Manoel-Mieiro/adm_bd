0c037f6ac3f235419a381f6c84f5d7d54ad1baf7
Manoel-Mieiro <mjlmieiro@gmail.com>
2024-04-17 15:00:30 -0300

    Continuação da lista da aula prática

---
+++ volume/L1/5.sql
@@ -0,0 +1,29 @@
+-- Crie um script de inserir 5 disciplinas
+INSERT INTO
+    disciplina(nome, descricao, carga_horaria)
+VALUES
+    (
+        'Análise e Modelagem',
+        'Modelagem de sistemas com UML',
+        72
+    ),
+    (
+        'Estrutura de Dados II',
+        'Sorting, binary search e árvores',
+        90
+    ),
+    (
+        'Sistemas Embarcados I',
+        'Programação de embarcados para ARDUINO UNO E ESP1286',
+        72
+    ),
+    (
+        'Sistemas Operacionais',
+         'Praticamente',
+          72
+    ),
+    (
+        'Banco de Dados',
+         'DER, MER e SQL',
+          90
+    );
